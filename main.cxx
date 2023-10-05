ed5140639936716f7622abae00a3109d96f77c48
img <paul.oshchepkov@icloud.com>
2023-10-06 00:43:56 +0200

    separated the key calculation to a function, also separated check for amount of input arguments into a bool

--- main.cxx
+++ main.cxx
@@ -2,24 +2,27 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+int CalculateKey(int _sum, std::string _inputStringArg1,  std::string _inputStringArg0){
+    size_t exeLen{_inputStringArg0.size()};
+    char firstChar{*(_inputStringArg1.data())};
+    return(int)((_sum ^ firstChar * 3) << (exeLen & 0x1f));
+}
+
 int main(int argCount, char *argValues[]) {
-    printf("%s", "Start");
-    if (argCount == 3) {
-        printf("%s", "if");
+    bool correctAmountOfArguments = (argCount == 3);
+    if (correctAmountOfArguments) {
         std::string executableName{argValues[0]};
         std::string arg1{argValues[1]};
-        char firstChar{*(argValues[1])};
-        size_t exeLen{executableName.size()};
         int arg2{std::atoi(argValues[2])};
         int sum = 0;
         for (int ptr = 0; ptr < (int)arg1.size();ptr++){
             sum += arg1[ptr];
         }
-        if ((sum ^ firstChar * 3) << (exeLen & 0x1f) == arg2) {
+        if (CalculateKey(sum, arg1, executableName) == arg2) {
         std::cout << "Correct!" << std::endl;
         } 
         else {
         std::cout << "Wrong!" << std::endl;
         }
-    }
+    } 
 }
