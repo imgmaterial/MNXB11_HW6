87c666fb0ec9ca7a7a891e07b2fa91e37d653055
img <paul.oshchepkov@icloud.com>
2023-10-06 00:24:49 +0200

    change while loop to for loop

--- main.cxx
+++ main.cxx
@@ -8,19 +8,14 @@ int main(int argCount, char *argValues[]) {
         printf("%s", "if");
         std::string executableName{argValues[0]};
         std::string arg1{argValues[1]};
-        char currentArg1Letter{*(argValues[1])};
+        char firstChar{*(argValues[1])};
         size_t exeLen{executableName.size()};
         int arg2{std::atoi(argValues[2])};
         int sum = 0;
-        int ptr = 0;
-        while (true) {
-            sum += arg1[ptr++];
-            if (sum >= static_cast<int>(arg1.size())) {
-                break;
-            }
+        for (int ptr = 0; ptr < (int)arg1.size();ptr++){
+            sum += arg1[ptr];
         }
-        printf("integer %lu\n", (exeLen & 0x1f));
-        if ((sum ^ currentArg1Letter * 3) << (exeLen & 0x1f) == arg2) {
+        if ((sum ^ firstChar * 3) << (exeLen & 0x1f) == arg2) {
         std::cout << "Correct!" << std::endl;
         } 
         else {
